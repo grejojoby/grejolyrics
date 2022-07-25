f2daa535f3e69b7fd1f7f8c9c630e675770fff59
Grejo Joby <51746129+grejojoby@users.noreply.github.com>
2022-07-25 14:12:11 +0530

    actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'grejojoby', // Usually your GitHub org/user name.
+  projectName: 'grejolyrics', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -57,9 +58,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Grejo Lyrics',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Grejo Lyrics',
           src: 'img/logo.svg',
         },
         items: [
